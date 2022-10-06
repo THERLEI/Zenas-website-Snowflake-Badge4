d1712e871f68e65b175451c61754c0007c50bdba
THERLEI <113339267+THERLEI@users.noreply.github.com>
2022-10-06 09:39:52 +0200

    Update zenas_app.py

--- zenas_app.py
+++ zenas_app.py
@@ -31,6 +31,9 @@ streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
